0728402dbbe4ca485e7a3bacd2010e8b112dce45
jxsl13 <jxsl13@gmail.com>
2020-07-24 05:10:52 +0200

    wrap split codeblocks

--- discordgo.go
+++ discordgo.go
@@ -1,16 +1,60 @@
 package main
 
-import "github.com/bwmarrin/discordgo"
+import (
+	"log"
+	"strings"
+
+	"github.com/bwmarrin/discordgo"
+)
 
 // SplitChannelMessageSend properly splits long output in order to accepted by the discord servers.
-func SplitChannelMessageSend(s *discordgo.Session, m *discordgo.MessageCreate, text string) error {
+// also properly wrap single codeblocks that were split during this process
+func SplitChannelMessageSend(s *discordgo.Session, m *discordgo.MessageCreate, text string) {
+	const codeblockDelimiter = "```"
+
+	codeblockFound := strings.Count(text, codeblockDelimiter) == 2
 	chunks := Split(text, "\n", 1800)
 
-	for _, chunk := range chunks {
-		if _, err := s.ChannelMessageSend(m.ChannelID, chunk); err != nil {
-			return err
+	codeblockBegin := -1
+	codeblockEnd := -1
+
+	if codeblockFound {
+		beginSet := false
+
+		for idx, chunk := range chunks {
+
+			delimiterCount := strings.Count(chunk, codeblockDelimiter)
+			if delimiterCount == 2 {
+				codeblockBegin = idx
+				codeblockEnd = idx
+				break
+			} else if delimiterCount == 1 && !beginSet {
+				codeblockBegin = idx
+				beginSet = true
+			} else if delimiterCount == 1 && beginSet {
+				codeblockEnd = idx
+				break
+			}
+
 		}
 	}
 
-	return nil
+	isCodeblockSplit := 0 <= codeblockBegin && codeblockBegin < codeblockEnd
+
+	for idx, chunk := range chunks {
+
+		if isCodeblockSplit {
+			if idx == codeblockBegin {
+				chunk = chunk + codeblockDelimiter
+			} else if codeblockBegin < idx && idx < codeblockEnd {
+				chunk = codeblockDelimiter + chunk + codeblockDelimiter
+			} else if idx == codeblockEnd {
+				chunk = codeblockDelimiter + chunk
+			}
+		}
+
+		if _, err := s.ChannelMessageSend(m.ChannelID, chunk); err != nil {
+			log.Println(err)
+		}
+	}
 }
