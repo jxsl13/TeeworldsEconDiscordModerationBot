8ce99ffddcf1419691aae2a6898dd6719ad71f00
jxsl13 <jxsl13@gmail.com>
2020-07-29 18:53:19 +0200

    remove not interceptible signals

--- main.go
+++ main.go
@@ -321,7 +321,7 @@ func main() {
 	// Wait here until CTRL-C or other term signal is received.
 	log.Println("Bot is now running.  Press CTRL-C to exit.")
 	sc := make(chan os.Signal, 1)
-	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt, os.Kill)
+	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM)
 	<-sc
 	globalCancel()
 
